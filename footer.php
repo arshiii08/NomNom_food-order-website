54d43ebb4f5ed5780637ef6940326d3874093cbb
arshiii08 <arshia.moudgil@gmail.com>
2025-03-20 10:33:11 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:arshia.moudgil@gmail.com">arshia.moudgil@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>12am to 12pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">jaipur, india - 303007</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:8699858219">86998-58219</a>
+      </div>
+
+   </section>
+
+   <div class="credit">By <span>Arshia Moudgil</span></div>
+
+</footer>
